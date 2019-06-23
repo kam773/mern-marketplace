c4a45d97838391e32bb17d99dfd4f0dc38605ef9
Kamil <developer.kamil90@gmail.com>
2019-06-23 15:20:34 +0200

    add

--- config/config.js
+++ config/config.js
@@ -2,5 +2,5 @@ module.exports = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-  mongoURI: 'mongodb://kamil:polska12@ds241647.mlab.com:41647/sellers'
+  mongoURI: process.env.MONGODB_URI
 }
